975d810ce2c652de34dd4f2996f306ef13df493c
soft091 <62936655+soft091@users.noreply.github.com>
2020-07-26 09:32:42 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,3 +15,23 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+# Modify default IP
+sed -i 's/192.168.1.1/192.168.118.254/g' package/base-files/files/bin/config_generate
+
+# Add a feed source
+# sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i '$a src-git diy https://github.com/firker/diy-ziyong' feeds.conf.default
+# sed -i '$a src-git jerryk https://github.com/jerrykuku/openwrt-package' feeds.conf.default
+git clone -b master https://github.com/vernesong/OpenClash.git package/OpenClash
+git clone -b 18.06 https://github.com/garypang13/luci-theme-edge.git package/luci-theme-edge
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
+git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
+svn co https://github.com/Lienol/openwrt-package/trunk/lienol/luci-app-passwall package/luci-app-passwall
+svn co https://github.com/Lienol/openwrt-package/trunk/package/brook package/brook
+svn co https://github.com/Lienol/openwrt-package/trunk/package/chinadns-ng package/chinadns-ng
+svn co https://github.com/Lienol/openwrt-package/trunk/package/tcping package/tcping
+svn co https://github.com/Lienol/openwrt-package/trunk/package/trojan-go package/trojan-go
+svn co https://github.com/Lienol/openwrt-package/trunk/package/syncthing package/syncthing
+mkdir dl && cd dl && wget https://static.adguard.com/adguardhome/release/AdGuardHome_linux_amd64.tar.gz && cd ..
+
